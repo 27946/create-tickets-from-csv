97269bb933e0e6db943553a618c13c0df75e2b29
27946 <aswinvayiravan@gmail.com>
2014-11-19 05:57:21 +0000

    Create create_tickets_from_csv.pl

---
+++ create_tickets_from_csv.pl
@@ -0,0 +1,60 @@
+use REST::Client;
+use MIME::Base64;
+
+
+$portal_url='';# yourdomain.freshdesk.com
+
+$email_id = ''; #You many alternately enter your API key here
+
+$email_pass = '';#You may enter 'X' here within quotes if you have entered API key
+
+$client = REST::Client->new();
+
+$message="My Message ";#put your message here
+$subject="New My Subject ";#put your subject here
+
+$file_name='mock.csv';#path the CSV files with one email in one line
+
+open(DATA,$file_name);
+
+@list;
+
+while(<DATA>)
+{
+  chomp($cur=$_);
+  push(@list,$cur);
+}
+print "Counting\n";
+$count=@list;
+
+print"The # of tickets that will be created $count\n";
+
+$client->setHost("https://$portal_url");
+$client->addHeader('Content-Type', 'application/json');
+$client->addHeader('charset', 'UTF-8');
+$client->addHeader("Authorization", "Basic ".encode_base64("$email_id:$email_pass"));
+
+
+for($i=0;$i<$count;$i++)
+{
+  $email="$list[$i]";
+  $req="
+  {
+    \"helpdesk_ticket\":{
+        \"description\":\"$message\",
+        \"subject\":\"$subject\",
+        \"email\":\"$email\",
+        \"priority\":1,
+        \"status\":2
+    },
+    \"cc_emails\":\"diaena\@freeshdesk.com\"
+  }";
+
+
+  #my $response = $client->POST("/helpdesk/tickets.json",$req);
+  $client->POST("/helpdesk/tickets.json",$req);
+}
+
+
+
+close(DATA);
